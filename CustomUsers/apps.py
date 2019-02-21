feaad30eb6a490d7f63fea4ebabd9ffcec5b4224
Deniska <47465987+deniska69@users.noreply.github.com>
2019-02-21 23:00:08 +0700

    yeah! extended user model now realy work!

--- CustomUsers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class CustomusersConfig(AppConfig):
-    name = 'Пользователи'
-    app_label = 'social_django'
--- UserProfiles/models.py
+++ UserProfiles/models.py
@@ -1,19 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
-from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import User
-
-class Profile(AbstractUser):
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name_n = models.CharField(max_length=50, null=False, blank=False, verbose_name=u'Имя:')
     name_p = models.CharField(max_length=50, null=False, blank=False, verbose_name=u'Отчество:')
     name_f = models.CharField(max_length=50, null=False, blank=False, verbose_name=u'Фамилия:')
     position = models.CharField(max_length=100, null=False, blank=False, verbose_name=u'Должность:')
     birthday = models.CharField(max_length=30,null=False, blank=True, verbose_name=u'Дата рождения:')
     phone = models.CharField(max_length=30, null=False, blank=True, verbose_name=u'Моб.номер:')
-    note = models.CharField(max_length=200, null=False, blank=True, verbose_name=u'Заметка:')
+    bio = models.CharField(max_length=1000, null=False, blank=True, verbose_name=u'Биография:')
 
     def __str__(self):
-        return self.name_f + ' ' + self.name_n + ' ' + self.name_p + ' (' + self.username + ')'
+        return self.name_f + ' ' + self.name_n + ' ' + self.name_p
 
     verbose_name = 'Пользователи'
