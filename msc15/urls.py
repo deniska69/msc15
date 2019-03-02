e907b1dce9c5637c748fe67610e39e08ab83adc0
Deniska <47465987+deniska69@users.noreply.github.com>
2019-03-02 13:34:35 +0700

    remade verty

--- msc15/urls.py
+++ msc15/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.views.generic import TemplateView
+from . import views
+#from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="include.html")),
+    #path('', TemplateView.as_view(template_name="include.html")),
+    path('', views.IndexView.as_view()),
 ]
